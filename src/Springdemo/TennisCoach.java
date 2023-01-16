c76bebde39f51d7d641f43b30f30920b1f46ad99
kariotisalex <kariotis.alexander@gmail.com>
2023-01-16 22:17:53 +0200

    16/1/23 7th push

--- src/Springdemo/TennisCoach.java
+++ src/Springdemo/TennisCoach.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope("prototype")
 public class TennisCoach implements Coach{
@@ -18,6 +21,20 @@ public class TennisCoach implements Coach{
     public TennisCoach() {
         System.out.println(">> TennisCoach : inside default constructor");
     }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println(">> TennisCoach: inside of doMyStartupStuff");
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
+    }
+
+
 /*
     // define a setter method
     @Autowired
